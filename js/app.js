350973c421a95fc3b2d3eaa9656b2034a4aafca9
madison kanna <madisonkanna@gmail.com>
2017-02-19 19:43:39 -0800

    Add markers data

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var initialMarkers = [
+	title: "California State Capital",
+	lat: 38.576946,
+	lng: -121.493626,
+	position: "1315 10th St, Sacramento, CA 95814"
+	},
+	title: "Sutter's Fort State Historic Park"
+	lat: 38.572534, 
+	lng: -121.471202
+	position: 2701 L St, Sacramento, CA 95816,
+	},
+
+];
+
+
