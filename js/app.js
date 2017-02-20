24205fa646feed5a4b682e045ad1d0c88a269b79
madison kanna <madisonkanna@gmail.com>
2017-02-19 19:54:32 -0800

    Fix typos

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 var initialMarkers = [
+	{
 	title: "California State Capital",
 	lat: 38.576946,
 	lng: -121.493626,
 	position: "1315 10th St, Sacramento, CA 95814"
 	},
-	title: "Sutter's Fort State Historic Park"
+	{
+	title: "Sutter's Fort State Historic Park",
 	lat: 38.572534, 
-	lng: -121.471202
-	position: 2701 L St, Sacramento, CA 95816,
-	},
+	lng: -121.471202,
+	position: "2701 L St, Sacramento, CA 95816"
+	}
 
 ];
 
