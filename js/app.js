27bd5289ef0426568deeb9ed61c7cef716146028
madison kanna <madisonkanna@gmail.com>
2017-02-21 23:53:59 -0800

    Use knockout to create observables

--- js/app.js
+++ js/app.js
@@ -172,8 +172,19 @@ var Landmark = function(data) {
 
 
 var listViewModel = function() {
-    this.listView = ko.observable();
-    //this.name = ko.observable();
+    var self = this;
+
+    this.landmarkList = ko.observableArray([]);
+
+    locations.forEach(function(landmarkItem){
+        self.landmarkList.push( new Landmark(landmarkItem) );
+        
+    })
+    this.currentLandmark = ko.observable( this.landmarkList() [0]);
+
+    this.setLandmark = function(clickedLandmark) {
+        self.currentLandmark(clickedLandmark);
+    }
 }
 
 
